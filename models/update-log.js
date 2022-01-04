b12f5892b2ab71d6f4a1fb11317851b8a1c63ac1
flash-wally-west <69054838+flash-wally-west@users.noreply.github.com>
2022-01-04 11:14:21 -0500

    Update update-log.js

--- models/update-log.js
+++ models/update-log.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
+    influencerID: Number,
+    influenceeID: Number,
     influencer: String,
     influencee: String,
     message: Number,
-    time: Number,
+    time: String,
 })
 
 module.exports=mongoose.model("update-log",schema);
