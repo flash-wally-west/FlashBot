6e6b93cbbe3e1e5bac6788d501d49ff8b0c9379f
flash-wally-west <69054838+flash-wally-west@users.noreply.github.com>
2022-01-04 11:08:27 -0500

    Update rating-log.js

--- models/rating-log.js
+++ models/rating-log.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
     reviewer: String,
     reviewee: String,
-    message: Number,
+    reviewername: String,
+    revieweename: String,
     time: Number,
 })
 
