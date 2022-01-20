2992b18d2fb125685f45ca6051eac07ec103d419
flash-wally-west <69054838+flash-wally-west@users.noreply.github.com>
2022-01-19 20:31:49 -0500

    Update star-vote.js

--- commands/star-vote.js
+++ commands/star-vote.js
@@ -17,9 +17,9 @@ module.exports.run = async(bot, message, args) => {
     }
     
     message.reply("Okay, listening for candidates")
-        .then( ()=> {
+        .then( async()=> {
             message.channel.awaitMessages({filter, max: 1, time: 30000, errors: ['time'] })
-                .then( collected => {
+                .then( async collected => {
                     if(collected.content==="end"){
                         return;
                     } else {
